43f42451b773cecdbfadc0c1d43e0668f097becd
Danilo Bruno <danilobsilv@gmail.com>
2023-04-17 22:58:00 -0400

    delete: testing key call and testing header

--- utils/football_api.py
+++ utils/football_api.py
@@ -40,14 +40,12 @@ class FootballData:
     def __init__(self, url, api_key, file_path):
         self.url = url
         self.api_key = api_key
-        self.testing_key = ""
         self.file_path = file_path
         self.headers = {"Authorization": f"Bearer {self.api_key}"} 
-        self.testing_headers = {"Authorization": f"Bearer {self.testing_key}"}
 
 
     def fetch_data(self):
-        response = requests.get(self.url, headers = self.testing_headers)
+        response = requests.get(self.url, headers = self.headers)
         data = json.loads(response.text)
         with open(self.file_path, "w") as json_file:
             json.dump(data, json_file, indent=4)
