7ef78ac038d6179f94f0139657071671c41df33a
Danilo Bruno <danilobsilv@gmail.com>
2023-04-19 23:00:20 -0400

    test: running the bot from the main

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from utils.football_api import FootballData
 from src.tokens import Tokens
+from src.bot_src import client
+
 
 tokens = Tokens()
 
-BOT_URL = tokens.url
-TESTING_KEY = tokens.testing_key
+# BOT_URL = tokens.url
+# TESTING_KEY = tokens.testing_key
 
-football = FootballData(BOT_URL, TESTING_KEY, "football-data\server_data.json")
-football.fetch_data()
+# football = FootballData(BOT_URL, TESTING_KEY, "football-data\server_data.json")
+# football.fetch_data()
+
+client.run(tokens.bot_token)
