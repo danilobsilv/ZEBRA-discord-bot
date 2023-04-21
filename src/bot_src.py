f0ce1afee98ea9044ca61a8897a4f913bc0138aa
Danilo Bruno <danilobsilv@gmail.com>
2023-04-20 20:45:31 -0400

    made it look better

--- src/bot_src.py
+++ src/bot_src.py
@@ -1,29 +1,44 @@
 import discord 
 import re 
-# from tokens import Tokens
-# from utils.functions.functions import *
 
-# token = Tokens()
 
 intents = discord.Intents.default()
 intents.message_content = True
 
+
 client = discord.Client(intents=intents)
-# def printarOi():
-#     return "Hello World!"
+
 @client.event
 async def on_ready():
     print(f"{client.user} is online!")
 
+
+'''
+the function is triggered whenever a new message is sent in a channel where the bot is present. 
+It verifies if the message begins with any of the predefined keywords in the responses dictionary, and if it does, 
+sends an appropriate response to the message channel.
+'''
 @client.event
 async def on_message(message):
     formated_message_author = re.sub("^#\d{1,10}$", "" , message.author.name)
+    
+    # treat the variations of the same word 
+    message_content = message.content
+    lower_message_content  = message_content.lower()
 
     if message.author == client.user:
         return 
-    
-    if message.content.startswith("#oi"):
-        # await message.channel.send(printar_oi()) funcionou desse jeito
-        await message.channel.send(f"Oi, {formated_message_author}, tudo bem?")
+
+    responses = {
+        "#oi": f"Oi, {formated_message_author}, tudo bem?",
+        "#danilo": "Danilo está ocupado me desenvolvendo...Para de encher o saco!",
+        "#fernanda": "Fernanda é a mãe dele!"
+    }
+
+    for keyword, response in responses.items():
+        if lower_message_content.startswith(keyword):
+            await message.channel.send(response)
+            break
+
 
 
