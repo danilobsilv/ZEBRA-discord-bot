9ef0fb7b9a6f4aa3e6719e26c17b1cfbf939e2b1
Danilo Bruno <danilobsilv@gmail.com>
2023-04-21 15:59:12 -0400

    deleting comments

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils.football_api import FootballData
 from config.tokens import Tokens
 from src.bot_src import client
 from utils.functions.functions import getTable
+
 tokens = Tokens()
 
 client.run(tokens.bot_token)
