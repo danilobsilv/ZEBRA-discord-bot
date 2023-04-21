6ceef00ccf6a7f6009053e6defc0f0ab5bb55e69
Danilo Bruno <danilobsilv@gmail.com>
2023-04-21 15:55:09 -0400

    response is writting straight into the json

--- utils/functions/functions.py
+++ utils/functions/functions.py
@@ -11,23 +11,22 @@ def sayOi(message):
     author_mention = message.author.mention
     return f"Oi, {author_mention},  como você tá?"
 
+
 def getTable():
-      url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
-      headers = {'Authorization': f'Bearer {tokens.api_key}'}
-      response = requests.get(url, headers=headers)
-      table = response.json()
+    url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
+    headers = {'Authorization': f'Bearer {tokens.api_key}'}
+    response = requests.get(url, headers=headers)
+    table = response.json()
 
-      with open("football-data\serieA_data.json", "w") as outfile:
-            json.dump(table, outfile, indent=4)
+    with open("football-data\serieA_data.json", "w") as outfile:
+        json.dump(table, outfile, indent=4)
 
-      with open('football-data\serieA_data.json', 'r') as f:
-            data = json.load(f)
-            formatted_table = ""
-            for team in data:
-                  popular_name = team['time']['nome_popular']
-                  position = team['posicao']
-                  points = team['pontos']
-                  row = f'{position}º {popular_name} -- {points} pontos\n'
-                  formatted_table += row
+    formatted_table = ""
+    for team in table:
+        popular_name = team['time']['nome_popular']
+        position = team['posicao']
+        points = team['pontos']
+        row = '{:2}º    {} -- {} pontos\n'.format(position, popular_name, points)
+        formatted_table += row
 
-            return formatted_table
+    return formatted_table
