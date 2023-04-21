72b4fd12751ef11fbacc9185235b90c219375890
Danilo Bruno <danilobsilv@gmail.com>
2023-04-21 15:58:57 -0400

    deleting comments

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 from utils.football_api import FootballData
 from config.tokens import Tokens
 from src.bot_src import client
-# from utils.functions.functions import getTabela()
 from utils.functions.functions import getTable
 tokens = Tokens()
 
-# BOT_URL = tokens.url
-# TESTING_KEY = tokens.testing_key
-
-# football = FootballData(BOT_URL, TESTING_KEY, "football-data\server_data.json")
-# football.fetch_data()
-# getTabela()
 client.run(tokens.bot_token)
