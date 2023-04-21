aa566d8c40719324b11ffae4187a3de7c2aafb6e
Danilo Bruno <danilobsilv@gmail.com>
2023-04-21 14:56:13 -0400

    now sayHi mentions the message author

--- utils/functions/functions.py
+++ utils/functions/functions.py
@@ -1,32 +1,35 @@
+import re
 import requests
 import json
+# from config.tokens import Tokens
 
+# tokens = Tokens()
 
 def commands():
       pass
 
-def sayOi():  # to trigger "#oi"
-      return f"Oi, como você tá?"
+def sayOi(message):
+    author_mention = message.author.mention
+    return f"Oi, {author_mention},  como você tá?"
 
-def getTabela():
+
+def getTabela():  # to do
 
       url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
 
-      # Substitua {sua_chave_de_acesso} pela chave de acesso fornecida pela API
+      
       headers = {'Authorization': f'Bearer {"live_2d712d4006ae19758252534d43d9ac"}'}
+      # headers = tokens.api_key
 
-      # Faz a solicitação GET para a API
       response = requests.get(url, headers=headers)
 
-      # Analisa a resposta JSON
+
       tabela = response.json()
 
-      # Escreve a tabela em um arquivo JSON
+
       with open("football-data\serieA_data.json", "w") as outfile:
             json.dump(tabela, outfile, indent=4)
 
-      # Faça o que desejar com a tabela obtida
-      print(tabela)
-
+getTabela()
 
 
