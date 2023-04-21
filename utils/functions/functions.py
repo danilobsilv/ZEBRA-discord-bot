3ab6fa4edade8542b868ac72935c721d92df852b
Danilo Bruno <danilobsilv@gmail.com>
2023-04-20 23:23:25 -0400

    will keep it structured

--- utils/functions/functions.py
+++ utils/functions/functions.py
@@ -1,18 +1,32 @@
-"""def printar_oi():
-      return "hello world!"
+import requests
+import json
 
-def printar_meu_nome():
-      return "Danilo Bruno da Silva"
-
-def qlqrCoisa():
-      return "Qualquer coisa"
-"""
-
-import re
-from discord import message
 
 def commands():
-    pass
+      pass
 
 def sayOi():  # to trigger "#oi"
-    return f"Oi, como você tá?"
+      return f"Oi, como você tá?"
+
+def getTabela():
+
+      url = 'https://api.api-futebol.com.br/v1/campeonatos/10/tabela'
+
+      # Substitua {sua_chave_de_acesso} pela chave de acesso fornecida pela API
+      headers = {'Authorization': f'Bearer {"live_2d712d4006ae19758252534d43d9ac"}'}
+
+      # Faz a solicitação GET para a API
+      response = requests.get(url, headers=headers)
+
+      # Analisa a resposta JSON
+      tabela = response.json()
+
+      # Escreve a tabela em um arquivo JSON
+      with open("football-data\serieA_data.json", "w") as outfile:
+            json.dump(tabela, outfile, indent=4)
+
+      # Faça o que desejar com a tabela obtida
+      print(tabela)
+
+
+
