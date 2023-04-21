77480c099690efd9840dc3cf23811ed60e79bc1b
Danilo Bruno <danilobsilv@gmail.com>
2023-04-20 22:43:50 -0400

    setup cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+      name = "ZEBRA discord bot",
+    
+      version = "1.0.0",
+    
+      author = "Danilo Bruno da Silva",
+    
+      description = """A discord bot that getters info about the about the brazilian football championship and allows the users,
+                              through simple commands, to see these info and stay tuned about what's new""",
+
+      install_requires = ["discord.py 2.2.2"]
+)
